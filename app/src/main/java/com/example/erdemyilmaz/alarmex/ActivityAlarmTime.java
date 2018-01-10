efed026adf9c118c3f497466806ccfd709e7fbf0
yilmazerd <yilmazerdem@gmail.com>
2018-01-09 20:43:49 -0500

    jan

--- app/src/main/java/com/example/erdemyilmaz/alarmex/ActivityAlarmTime.java
+++ app/src/main/java/com/example/erdemyilmaz/alarmex/ActivityAlarmTime.java
@@ -1,7 +1,16 @@
 package com.example.erdemyilmaz.alarmex;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ActivityAlarmTime extends AppCompatActivity {
 
@@ -9,5 +18,89 @@ public class ActivityAlarmTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_time);
+        final SharedPreferences sp = getSharedPreferences("label",0);
+        final SharedPreferences.Editor editor = sp.edit();
+
+
+
+
+        Button myButton = (Button)(findViewById(R.id.BUTTON_SAVE_ALARMT));
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TextView tvHour = (TextView)(findViewById(R.id.textSetHour));
+                TextView tvMinute = (TextView)(findViewById(R.id.textSetMinute));
+
+                String aHour = tvMinute.getText().toString();
+                String aMinute = tvHour.getText().toString();
+
+                int Hour = Integer.parseInt(aHour);
+                int Minute = Integer.parseInt(aMinute);
+
+                String[] ns = new String[2];
+                ns[0]="ETTE";
+                ns[1]="EKKE";
+
+                //final String msString = mPrefs.getString("tag1","Load default");
+
+                //SharedPreferences sp = getSharedPreferences("AlarmTimes", Activity.MODE_PRIVATE);
+
+
+                String mm = sp.getString("turgut","Load default");
+                Log.e("TAG--",mm);
+
+                editor.putString("turgut","palla");
+
+                mm = sp.getString("turgut","Load default");
+                Log.e("TAG--",mm);
+
+                //editor.putInt("RecordMinute", Minute);
+                //editor.putInt("MinuteCounter", Minute);
+                //editor.putInt("RecordHour", Hour);
+                //editor.commit();
+            }
+        });
+
+
     }
+
+    public String generateKeyName (String key, int k) {
+        String pass1 = "Key:" + key + "-" + String.valueOf(k);
+        return  pass1;
+    };
+
+    public  Integer getKeyNumber (String keyName) {
+        String example = keyName.substring((keyName.lastIndexOf("-") + 1 ));
+        return Integer.parseInt(example);
+    }
+
+    public void  enterNewValue (String tag, int newVal, SharedPreferences shp) {
+        // TODO GET THE NUMBER OF ENTRIES IN THE EXISTING TAG
+        int numOfEntries = getKeyNumber(tag);
+        Log.e("KSS-",String.valueOf(numOfEntries));
+
+        // ANOTHER VARIABLE WILL BE IF THE EXISTING TAG IS ENTERED BEFORE
+
+        // TODO INCREASE THE NUMBER OF ENTRIES IN THE LOG, MAKE THE ENTRY
+    }
+
+    public ArrayList<String> getTheValue (String tag, SharedPreferences shp) {
+        ArrayList<String> ar = new ArrayList<String>();
+        //
+        // TODO use the tag, get the size of the array in the database, write the database into arraylist
+        //
+        int numOfEntries = getKeyNumber(tag);
+        for (int i=0;i<numOfEntries;i++) {
+            String nmn = generateKeyName(tag,i);
+            String msString = shp.getString(nmn,"Load default");
+            ar.add(msString);
+            Log.e("KSS-",msString);
+        }
+        return ar;
+    }
+
+
 }
+
--- app/src/main/java/com/example/erdemyilmaz/alarmex/MainActivity.java
+++ app/src/main/java/com/example/erdemyilmaz/alarmex/MainActivity.java
@@ -8,22 +8,53 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 import java.util.ArrayList;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private boolean inTheScreen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        inTheScreen = true;
 
         setContentView(R.layout.activity_main);
-        int[] myDelays = {5000};
+//        int[] myDelays = {5000};
+//        scheduleService(myDelays);
+
+        int[] myDelays = {5000,25000};
         scheduleService(myDelays);
+
         Log.e("TAG--","Ordering services");
 
+        Button myButton = (Button)(findViewById(R.id.buttonRetrieve));
 
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        inTheScreen = true;
+    }
+
+
+    @Override
+    public void onUserInteraction(){
+        int[] myDelays = {5000};
+        scheduleService(myDelays);
+        Log.e("TAG--","User did something");
     }
 
     private void scheduleService(int[] delays) {
@@ -40,10 +71,13 @@ public class MainActivity extends AppCompatActivity {
             PendingIntent pendingIntent = PendingIntent.getBroadcast(this, i, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
             PE.add(pendingIntent);
             alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis + delays[i], PE.get(i));
+            //alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1515547380000, PE.get(i));
         }
 
         Log.e("TAG--","Set alarm manager");
     }
+
+
 
 
 
